b28ccbfccb680360e7a444758441ab9cf2f918c2
KING DULA 2M <dulanthikshana69@gmail.com>
2026-05-08 13:16:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const os = require('os')
 
 // --- Configuration ---
 const OWNER_NAME = "KING-DULA"; 
-const PHONE_NUMBER = "94787535242"; 
+const PHONE_NUMBER = "94719251323"; 
 const BASE_API = "https://api.giftedtech.my.id/api"; 
 const API_KEY = "gifted"; 
 
